39ab7ebb2a30b68b3a6b727837e582c4ea7cd106
kevim <kevim@kevim-server>
2014-04-12 18:11:11 -0300

    Modificando para funcionar no Tomcat 8

--- src/main/java/com/pizzaria/restaurante/action/LoginAction.java
+++ src/main/java/com/pizzaria/restaurante/action/LoginAction.java
@@ -3,26 +3,42 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.pizzaria.restaurante.action;
 
 import java.io.Serializable;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.hibernate.Session;
 
 /**
  *
  * @author kevim
  */
 @ManagedBean(name = "login")
-public class LoginAction implements Serializable{
+public class LoginAction implements Serializable {
+
     private String testeBean;
     private String valorBanco;
-    
+    private EntityManagerFactory entityManagerFactory;
+
     @PostConstruct
-    public void init(){
+    public void init() {
         setTesteBean("helll yeah bitches");
-    
+        setUp();
+        
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        Session session = entityManager.unwrap(org.hibernate.Session.class);
+        session.createSQLQuery("select * from users");
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+
+    protected void setUp(){
+        entityManagerFactory = Persistence.createEntityManagerFactory("restaurante-pu");
     }
 
     public String getTesteBean() {
@@ -40,6 +56,5 @@ public class LoginAction implements Serializable{
     public void setValorBanco(String valorBanco) {
         this.valorBanco = valorBanco;
     }
-    
-    
+
 }
