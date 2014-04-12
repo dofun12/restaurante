cc29b634650a693a89c35fcf00fb1fbf3900ce06
kevim <kevim@kevim-server>
2014-04-12 18:16:41 -0300

    Primeiro Select

--- src/main/java/com/pizzaria/restaurante/action/LoginAction.java
+++ src/main/java/com/pizzaria/restaurante/action/LoginAction.java
@@ -6,12 +6,17 @@
 package com.pizzaria.restaurante.action;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.xml.crypto.dsig.Transform;
+import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.transform.Transformers;
 
 /**
  *
@@ -23,7 +28,8 @@ public class LoginAction implements Serializable {
     private String testeBean;
     private String valorBanco;
     private EntityManagerFactory entityManagerFactory;
-
+    private List<Map<String,Object>> result;
+    
     @PostConstruct
     public void init() {
         setTesteBean("helll yeah bitches");
@@ -32,7 +38,9 @@ public class LoginAction implements Serializable {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         entityManager.getTransaction().begin();
         Session session = entityManager.unwrap(org.hibernate.Session.class);
-        session.createSQLQuery("select * from users");
+        Query q = session.createSQLQuery("select * from users");
+        q.setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP);
+        result = q.list();
         entityManager.getTransaction().commit();
         entityManager.close();
     }
@@ -57,4 +65,12 @@ public class LoginAction implements Serializable {
         this.valorBanco = valorBanco;
     }
 
+    public List<Map<String, Object>> getResult() {
+        return result;
+    }
+
+    public void setResult(List<Map<String, Object>> result) {
+        this.result = result;
+    }
+    
 }
